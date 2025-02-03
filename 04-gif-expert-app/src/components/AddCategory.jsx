9c3aa3d68cb8af74997f042e2ea48a65d934ecb4
Juan Bermudez <jbermudez@cinesunidos.com>
2025-02-03 11:34:37 -0400

    Fix

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -7,8 +7,7 @@ export const AddCategory = ( { onNewCategory } ) => {
     };
     const onSubmit = (event) => { 
         event.preventDefault();   
-        if( inputValue.trim().length <= 1 ) return;     
-        //onAddCategory( categories => [ inputValue, ...categories ]);
+        if( inputValue.trim().length <= 1 ) return;       
         onNewCategory( inputValue.trim() );
         setInputValue('');
     }; 
